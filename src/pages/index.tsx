e00d19f4e767fcc9d9ca37f848ea6417ee4d90d2
Matt Greer <matt.e.greer@gmail.com>
2021-07-16 19:42:07 -0400

    remove index/

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { IndexPage } from '../components/index/IndexPage';
+import { IndexPage } from '../components/IndexPage';
 
 function NextIndexPage() {
 	return <IndexPage />;
 }
 
 export default NextIndexPage;
-
