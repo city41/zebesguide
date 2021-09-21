538aa210c25d1bee1f416975e4015acfcd0f29a5
Matt Greer <matt.e.greer@gmail.com>
2021-09-21 09:23:57 -0400

    IndexPage: stop dumping parse save file

--- src/components/IndexPage/IndexPage.tsx
+++ src/components/IndexPage/IndexPage.tsx
@@ -1,20 +1,15 @@
 import { useEffect, useState } from 'react';
 import { DropZone } from '../DropZone';
-import { parse } from '../../lib/parser';
-import type { SaveFile } from '../../lib/parser';
 import { ZebesMap } from './ZebesMap';
 import { getStartingCells } from '../../lib/parser/cells/getStartingCells';
 import { parseCells } from '../../lib/parser/cells/parseCells';
 
 function IndexPage() {
 	const [data, setData] = useState<null | Uint8Array>(null);
-	const [parsedSaveFile, setParsedSaveFile] = useState<null | SaveFile>(null);
 	const [cells, setCells] = useState<CellMatrix>(getStartingCells());
 
 	useEffect(() => {
 		if (data) {
-			const parsed = parse(data);
-			setParsedSaveFile(parsed);
 			const saveCells = parseCells(data);
 			setCells(saveCells);
 		}
@@ -23,7 +18,6 @@ function IndexPage() {
 	return (
 		<div className="w-32 h-32">
 			<DropZone onData={(f) => setData(f)} />
-			{parsedSaveFile && <pre>{JSON.stringify(parsedSaveFile, null, 2)}</pre>}
 			<ZebesMap matrix={cells} />
 		</div>
 	);
