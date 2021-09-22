eec473ff7e0b2fd32b6fd0587f2945c92afccb0e
Matt Greer <matt.e.greer@gmail.com>
2021-09-22 11:16:51 -0400

    brinstar0 cells added

--- src/lib/parser/cells/mapBits.ts
+++ src/lib/parser/cells/mapBits.ts
@@ -1,14 +1,17 @@
 import { crateria0 } from './crateria0';
 import { crateria1 } from './crateria1';
+import { brinstar0 } from './brinstar0';
+
 import uniqBy from 'lodash/uniqBy';
 
-const cells = uniqBy(
-	crateria0.concat(crateria1),
-	(c) => `${c.mapCell.x}-${c.mapCell.y}`
-);
+const inputs = [crateria0, crateria1, brinstar0];
 
-console.log('crateria0 count', crateria0.length);
-console.log('crateria1 count', crateria1.length);
+const nonDedupedCells = inputs.reduce((building, input) => {
+	console.log(input[0].area, input[0].savePoint, input.length);
+	return building.concat(input);
+}, []);
+
+const cells = uniqBy(nonDedupedCells, (c) => `${c.mapCell.x}-${c.mapCell.y}`);
 console.log('total cells', cells.length);
 
 export { cells };
