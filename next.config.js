107d67f88e3bf028fb3f2be5c21f49fa69d6bf14
Matt Greer <matt.e.greer@gmail.com>
2021-10-14 17:06:13 -0400

    next.config: remove redirects, classname optimization

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const withPlugins = require('next-compose-plugins');
 const withOptimizedImages = require('next-optimized-images');
-const withOptimizedClassnames = require('next-optimized-classnames');
 
 module.exports = withPlugins(
 	[
-		withOptimizedClassnames,
 		[
 			withOptimizedImages,
 			{
@@ -14,48 +12,11 @@ module.exports = withPlugins(
 		],
 	],
 	{
-		redirects() {
-			return [
-				{
-					source: '/make',
-					destination: '/editor',
-					permanent: true,
-				},
-				{
-					source: '/make/:id',
-					destination: '/editor/:id',
-					permanent: true,
-				},
-				{
-					source: '/make/:id/:slug',
-					destination: '/editor/:id/:slug',
-					permanent: true,
-				},
-				{
-					source: '/make/',
-					destination: '/editor',
-					permanent: true,
-				},
-				{
-					source: '/make/:id/',
-					destination: '/editor/:id',
-					permanent: true,
-				},
-				{
-					source: '/make/:id/:slug/',
-					destination: '/editor/:id/:slug',
-					permanent: true,
-				},
-			];
-		},
 		pageExtensions: ['tsx'],
 		trailingSlash: true,
 		publicRuntimeConfig: {
 			PROJECT_ROOT: __dirname,
 			ROOT_DOMAIN: 'zebesguide.com',
 		},
-		// images: {
-		// 	disableStaticImages: true,
-		// },
 	}
 );
