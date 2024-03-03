20c3037c54c81368c99076e03cb71c4992a0534a
Matt Greer <matt.e.greer@gmail.com>
2024-03-02 21:35:54 -0500

    norfairm: add remaining hallway

--- src/lib/parser/cells/norfairm.ts
+++ src/lib/parser/cells/norfairm.ts
@@ -8,4 +8,58 @@ export const norfairm = [
 		byte: 0x20f,
 		bit: 3,
 	},
+	{
+		area: 'norfair',
+		mapCell: {
+			x: 59,
+			y: 41,
+		},
+		byte: 0x1f6,
+		bit: 7,
+	},
+	{
+		area: 'norfair',
+		mapCell: {
+			x: 60,
+			y: 41,
+		},
+		byte: 0x1f6,
+		bit: 6,
+	},
+	{
+		area: 'norfair',
+		mapCell: {
+			x: 61,
+			y: 41,
+		},
+		byte: 0x1f6,
+		bit: 5,
+	},
+	{
+		area: 'norfair',
+		mapCell: {
+			x: 62,
+			y: 41,
+		},
+		byte: 0x1f6,
+		bit: 4,
+	},
+	{
+		area: 'norfair',
+		mapCell: {
+			x: 63,
+			y: 41,
+		},
+		byte: 0x1f6,
+		bit: 3,
+	},
+	{
+		area: 'norfair',
+		mapCell: {
+			x: 64,
+			y: 41,
+		},
+		byte: 0x1f6,
+		bit: 2,
+	},
 ];
